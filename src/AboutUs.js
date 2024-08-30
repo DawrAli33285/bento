1d3036cfd7f633392154d9c54089691fc9cf7e85
DawrAli33285 <lemightyeagle@gmail.com>
2024-08-30 14:11:50 +0500

    updates

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 export default function AboutUs() {
     let [currentUSer,setCurrentUser]=useState("")
     useEffect(()=>{
+        window.scrollTo(0,0)
 setCurrentUser(JSON.parse(localStorage.getItem('user')))
     },[])
     let navigate=useNavigate();
